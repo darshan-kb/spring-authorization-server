268f0f3cd45519cbb9926cad88a71ae55c984e40
Darshan <darshan.kb@outlook.com>
2023-12-12 17:58:01 +0530

    added slotclient uri in application.properties

--- src/main/java/com/spin/authorizationserver/config/CORSCustomizer.java
+++ src/main/java/com/spin/authorizationserver/config/CORSCustomizer.java
@@ -1,5 +1,6 @@
 package com.spin.authorizationserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,12 +10,14 @@ import java.util.List;
 
 @Component
 public class CORSCustomizer {
+    @Value("${slotclienturi}")
+    String slotClientUri;
     public void corsCustomizer(HttpSecurity http) throws Exception{
         http.cors(c->{
             CorsConfigurationSource source = s ->{
                 CorsConfiguration cc = new CorsConfiguration();
                 cc.setAllowCredentials(true);
-                cc.setAllowedOrigins(List.of("http://localhost:3000"));
+                cc.setAllowedOrigins(List.of("http://localhost:3000",slotClientUri));
                 cc.setAllowedHeaders(List.of("*"));
                 cc.setAllowedMethods(List.of("*"));
                 return cc;
